9062752aabec20688c063f448d9ed1f74f5a8453
sandrineuwumukiza <uwasandrine99@gmail.com>
2024-02-20 11:44:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+var library = [
+    {
+        title:'Holy book',
+        author:'John',
+        availability:false
+    },
+    {
+        title:'Henriet',
+        author:'Henriet Bonte',
+        availability:true
+    },
+    {
+        title:'Atomic Habits',
+        author:'James Clair',
+        availability:true
+    },
+    {
+        title:'man of people',
+        author:'Ngugi'
+    }
+]
+const addBook = function
